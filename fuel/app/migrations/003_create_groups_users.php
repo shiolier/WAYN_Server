e35747f56a5af02bae3f2b410639a1f16bbbd17b
Haruto Tanaka <dev.htanaka@gmail.com>
2015-02-21 23:34:56 +0900

    #1 oil g model groups_users

---
+++ fuel/app/migrations/003_create_groups_users.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Fuel\Migrations;
+
+class Create_groups_users
+{
+	public function up()
+	{
+		\DBUtil::create_table('groups_users', array(
+			'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
+			'user_id' => array('constraint' => 11, 'type' => 'int', 'unsigned' => true),
+			'group_id' => array('constraint' => 11, 'type' => 'int', 'unsigned' => true),
+			'approve' => array('type' => 'bool', 'null' => true),
+			'created_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+			'updated_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+
+		), array('id'));
+	}
+
+	public function down()
+	{
+		\DBUtil::drop_table('groups_users');
+	}
+}
