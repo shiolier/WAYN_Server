f0fce7709bc963ab89ee3df6c3aab0e9f3116db4
Haruto Tanaka <dev.htanaka@gmail.com>
2015-02-21 22:58:52 +0900

    #1 Model_Base作成

---
+++ fuel/app/classes/model/base.php
@@ -0,0 +1,30 @@
+<?php
+
+use Orm\Model;
+
+class Model_Base extends Model {
+	const DATE_FORMAT = 'Y/m/d H:i:s';
+
+	public function created_at($format = null) {
+		return $this->date_format('created_at', $format);
+	}
+	public function updated_at($format = null) {
+		return $this->date_format('updated_at', $format);
+	}
+	public function deleted_at($format = null) {
+		return $this->date_format('deleted_at', $format);
+	}
+
+	public function date_format($key, $format = null) {
+		if(empty($this->$key)) {
+			return null;
+		}
+		if($format === false) {
+			return $this->$key;
+		}
+		if(empty($format)) {
+			$format = static::DATE_FORMAT;
+		}
+		return date($format, $this->$key);
+	}
+}
